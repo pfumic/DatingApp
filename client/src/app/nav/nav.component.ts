d79b786450a37e8377ef4da99124b806b0b30ee3
pfumic <patrik.fumic@gmail.com>
2022-07-06 12:31:35 +0200

    End of section 7 - implemented global error handling in both the API and the Angular app

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,9 +22,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(response =>{
       this.router.navigateByUrl('/members');
         console.log(response);
-      }, error => {
-        console.log(error);
-        this.toastr.error(error.error);
       })
   }
 
